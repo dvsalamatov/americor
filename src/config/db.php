5445ae7d9e0f95221fd0487d94bc7401acbcc3f6
Salamatov Dmitry <dmitriy.salamatov@rtmis.ru>
2022-08-15 10:10:31 +0300

    init

---
+++ src/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql;dbname=dbname',
+    'username' => 'root',
+    'password' => 'rootPassword',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
